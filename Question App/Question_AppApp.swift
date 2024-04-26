9bd09b770d749aa58aa2eb1a378a6aaa94384fbf
Alexandra Baer Chan <mingbaer@gmail.com>
2024-04-25 21:29:34 -0500

    Initial Commit

---
+++ Question App/Question_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Question_AppApp.swift
+//  Question App
+//
+//  Created by Alexandra Baer Chan on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Question_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
